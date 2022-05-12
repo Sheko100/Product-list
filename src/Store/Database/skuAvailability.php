d44c49d3d0dbfae043650f578ac83e57c0509423
Shaker Sharabi <sheko100@icloud.com>
2022-05-12 19:28:09 +0200

    added the required file for sku uniqueness checking

---
+++ src/Store/Database/skuAvailability.php
@@ -0,0 +1,11 @@
+<?php 
+
+$sku = $_GET["sku"];
+
+require_once("DatabaseManager.php");
+$db =  new \Store\Database\DatabaseManager();
+$db->connect();
+$isAvailable = $db->checkAvailabilityOf($sku);
+
+echo json_encode($isAvailable);
+?>
